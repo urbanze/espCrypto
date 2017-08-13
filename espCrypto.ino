519cf577b17d6e35c5cde6d08130d57f2f205f3d
urbanze <jvitor-2005@hotmail.com>
2017-08-13 20:35:13 -0300

    Update espCrypto.ino

--- espCrypto.ino
+++ espCrypto.ino
@@ -2,8 +2,6 @@
  Name:		espCrypto.ino
  Created:	13/08/2017 19:34:34
  Author:	José morais
- 
- Exemplo de encriptação usando espCrypto.
 */
 
 #include <EspCrypto.h>
@@ -13,13 +11,21 @@ espCrypto esp;
 
 void setup()
 {
+	String enc, dec;
+
 	Serial.begin(115200);//Inicia a comunicação serial.
 
 
-	esp.setKey("1234567890123456");//Adiciona a chave para encriptação, mantenha isto em segredo!
+	esp.setKey("1234567890123456");//Adiciona a chave para encriptação, mantenha em segredo!
+	
 
+	enc = esp.encrypt("oi");//A função encrypt retorna uma string encriptada.
+	Serial.println(enc);//Printa o criptograma (mensagem encriptada).
 
-	Serial.println(esp.encrypt("oi"));//Printamos a String criptografada retornada pela função encrypt.
+	dec = esp.decrypt(enc);//A função decrypt retorna uma string desencriptada.
+	Serial.println(dec);//Printa a mensagem desencriptada.
+	
+	
 }
 
 
